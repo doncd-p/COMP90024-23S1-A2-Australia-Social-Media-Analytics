68fd90ce1d0667b94edb23b57fa4ad2bd50c6604
Chenyang Dong <zjgdcy@gmail.com>
2023-05-06 23:12:13 +1000

    feat: Add couchdb client

--- src/backend/app/__init__.py
+++ src/backend/app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from .api import init_api
-from .utils import init_utils
 from .app_config import Config
 
 def create_app():
@@ -11,6 +10,5 @@ def create_app():
     # Initialize all the helpers to the Flask app
     with app.app_context():
         init_api(app)
-        init_utils(app)
     
     return app
