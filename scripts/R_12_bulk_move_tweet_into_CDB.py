195b7664f9f3b0cdf805fdaff06b6c4a3c345179
Lang (Ron) Chen <Ronstuckathome@gmail.com>
2023-05-12 14:49:16 +1000

    added comment to bulk move tweet into cdb

--- scripts/R_12_bulk_move_tweet_into_CDB.py
+++ scripts/R_12_bulk_move_tweet_into_CDB.py
@@ -9,11 +9,14 @@ database = sys.argv[3]
 username = sys.argv[4]
 password = sys.argv[5]
 
+# load the data
 with open(file_location, 'r') as f:
     twitter_cdb_bulk = json.load(f)
 
+# create the database
 subprocess.run(f'curl -X PUT http://{ip}:5984/{database} -u {username}:{password}', shell=True)
 
+# uplaod 10000 at a time
 for i in range(len(twitter_cdb_bulk['docs'])//10000):
     out = {'docs': twitter_cdb_bulk['docs'][i*10000:(i+1)*10000]}
     json_data = json.dumps(out).encode('utf-8')
