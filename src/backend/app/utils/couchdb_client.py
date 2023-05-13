44ae09fa55f35a668a5587e59ab9fb51059d3fec
Chenyang Dong <zjgdcy@gmail.com>
2023-05-14 00:52:00 +1000

    fix: couchdb client changed

--- src/backend/app/utils/couchdb_client.py
+++ src/backend/app/utils/couchdb_client.py
@@ -3,19 +3,33 @@ from ..app_config import Config
 
 class CouchDBClient:
     def __init__(self):
-        self.servers = [couchdb.Server(url) for url in Config.couchdb_urls()]
+        self.server = self.connect_to_couchdb()
         self.databases = {}
-
-        for server in self.servers:
-            for db_name in Config.COUCHDB_DATABASES:
-                try:
-                    db = server[db_name]
-                    self.databases[db_name] = db
-                except couchdb.ResourceNotFound:
-                    pass
+        
+        for db_name in self.server:
+            try:
+                db = self.server[db_name]
+                self.databases[db_name] = db
+            except couchdb.ResourceNotFound:
+                pass
+                
+    def connect_to_couchdb(self):
+        for url in Config.couchdb_urls():
+            try:
+                server = couchdb.Server(url)
+                # Try to access the server to check if it's up
+                server.version() 
+                return server
+            except:
+                # If the server is down, an exception will be raised
+                # and the next server in the list will be tried.
+                pass
+        raise Exception("No available CouchDB servers found")
 
     def get_db(self, db_name):
         if db_name not in self.databases:
             raise ValueError(f"Unknown database: {db_name}")
 
         return self.databases[db_name]
+
+client = CouchDBClient()
