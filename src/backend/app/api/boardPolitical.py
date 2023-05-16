a7b2aa0081046a0abd72c2546dc030d9ed98048d
Chenyang Dong <zjgdcy@gmail.com>
2023-05-17 06:08:56 +1000

    feat: New API for dashboard winning change vs sentiment

--- src/backend/app/api/boardPolitical.py
+++ src/backend/app/api/boardPolitical.py
@@ -1,8 +1,11 @@
 from flask import jsonify, request
 from flask_restful import Resource
+from flask import current_app as app
 from app.utils.couchdb_client import client
+import requests
+
 import calendar
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class BoardDailyAvgSentimentPolitical(Resource):
@@ -192,3 +195,88 @@ class BoardMonthlyAvgSentimentPolitical(Resource):
             )
             response.status_code = 500
             return response
+
+
+class BoardWinningChangeSentiment(Resource):
+    def get(self):
+        try:
+            type = request.args.get('type', 'week')
+            after = request.args.get('after')
+
+            # Check if the 'after' parameter is valid for number of certain time unit after election
+            if after is None or after == '' or int(after) <= 0:
+                response = jsonify(
+                    code=400,
+                    msg="Missing or invalid 'after' parameter. Expected a number greater than 0.",
+                    data={},
+                )
+                response.status_code = 400
+                return response
+
+            if type == 'week' or 'month':
+                sentiment_data = self.getSentimentAll(after, type)['data']
+                sudo_data = self.getSudoData()['data']
+                res = self.mergeResponses(sentiment_data, sudo_data)
+
+                response = jsonify(
+                    code=200,
+                    msg="ok",
+                    data=res,
+                )
+                response.status_code = 200
+                return response
+            else:
+                response = jsonify(
+                    code=400,
+                    msg="Invalid 'type' parameter. Expected 'day' or 'month'",
+                    data={},
+                )
+                response.status_code = 400
+                return response
+
+        except Exception as e:
+            response = jsonify(
+                code=500,
+                msg="Error occurred: " + str(e),
+                data={},
+            )
+            response.status_code = 500
+            return response
+
+    def getSentimentAll(self, after, type):
+        if type == 'week':
+            base_url = app.config['BASE_URL']
+            params = {'startweek': after, 'endweek': after}
+            res = requests.get(
+                f'{base_url}/political/sentiments/weekly', params=params).json()
+        if type == 'month':
+            base_url = app.config['BASE_URL']
+
+            election_date = datetime(2022, 5, 21)
+            # Assuming 30 days per month
+            delta_start = timedelta(days=(int(after)-1) * 30)
+            delta_end = timedelta(days=int(after) * 30)
+            start_date = (election_date + delta_start).strftime("%Y-%m-%d")
+            end_date = (election_date + delta_end).strftime("%Y-%m-%d")
+
+            params = {'startdate': start_date, 'enddate': end_date}
+            res = requests.get(
+                f'{base_url}/political/sentiments/monthly', params=params).json()
+
+        return res
+
+    def getSudoData(self):
+        base_url = app.config['BASE_URL']
+        res = requests.get(f'{base_url}/electorate/sudo_data/all').json()
+        return res
+
+    def mergeResponses(self, sentiment_data, sudo_data):
+        merged_data = {}
+
+        for electorate, sentiment in sentiment_data.items():
+            merged_data[electorate] = {
+                "avg_sentiment": sentiment["avg_sentiment"],
+                "hasChangedWinningParty": sudo_data[electorate].get("hasChangedWinningParty", 0)
+            }
+
+        return merged_data
