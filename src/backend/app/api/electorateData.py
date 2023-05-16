a59ad5788971a0f5382cde1ad2cde516a44eb191
Chenyang Dong <zjgdcy@gmail.com>
2023-05-17 06:07:51 +1000

    feat: New API for retrieving sudo data for all electorate

--- src/backend/app/api/electorateData.py
+++ src/backend/app/api/electorateData.py
@@ -3,7 +3,33 @@ from flask_restful import Resource
 from app.utils.couchdb_client import client
 
 
-class ElectorateData(Resource):
+class ElectorateGeoData(Resource):
+    def get(self):
+        db = client.get_db('geo_electorate')
+
+        view = db.view('_all_docs', limit=1, include_docs=True)
+
+        for row in view:
+            doc = row['doc']
+            break
+
+        if doc:
+            response = jsonify(
+                doc
+            )
+            response.status_code = 200
+            return response
+        else:
+            response = jsonify(
+                code=404,
+                msg="Document not found",
+                data={}
+            )
+            response.status_code = 404
+            return response
+
+
+class ElectorateSudoData(Resource):
     def get(self):
         electorate_filter = request.args.get('electorate')
 
@@ -40,19 +66,26 @@ class ElectorateData(Resource):
             return response
 
 
-class ElectorateGeoData(Resource):
+class ElectorateSudoDataAll(Resource):
     def get(self):
         db = client.get_db('geo_electorate')
 
         view = db.view('_all_docs', limit=1, include_docs=True)
 
-        for row in view:
-            doc = row['doc']
-            break
+        data_dict = {}
 
-        if doc:
+        for row in view:
+            features = row['doc']['features']
+            for feature in features:
+                properties = feature['properties']
+                division_name = properties.pop('_id')
+                data_dict[division_name] = properties
+
+        if data_dict:
             response = jsonify(
-                doc
+                code=200,
+                msg="ok",
+                data=data_dict
             )
             response.status_code = 200
             return response
