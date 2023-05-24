b42f59b03d192902dd3850d518c2c0b511f97401
andersonkam <andersonkam646@gmail.com>
2023-05-25 02:29:15 +1000

    fix: bug of ip

--- harvest/Mastodon.py
+++ harvest/Mastodon.py
@@ -11,14 +11,15 @@ import re
 import subprocess
 import os
 
-
+print(os.getenv('COUCHDB_IP'))
 # authentication
 admin = 'admin'
 password = 'password'
 database = 'toot_database'
-url = f"http://{os.getenv('COUCHDB_IP')}:5984/"
+couchdb_ip = os.getenv('COUCHDB_IP')
+url = f"http://{couchdb_ip}:5984/"
 # get couchdb instance
-couch = couchdb.Server(f"http://{admin}:{password}@{os.getenv('COUCHDB_IP')}:5984/")
+couch = couchdb.Server(f"http://{admin}:{password}@{couchdb_ip}:5984/")
 
 # indicate the db name
 db_names = ['toot_database']
